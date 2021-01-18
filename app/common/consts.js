beafbfe5a7aa3462af5d60899ff8a7b79138f37c
cxx <469350450@qq.com>
2021-01-18 22:57:27 +0800

    clean code

--- app/common/consts.js
+++ app/common/consts.js
@@ -1,22 +1,37 @@
 /**
- * Date: 2019/2/11
+ * Date: 2019/8/29
  * Author: admin
- * Description: 服务器专用consts
+ * Description: 常量文件
  */
 'use strict';
-var uconsts = require('../public/consts');
 
-var consts = {
-	APP_ID: "wxea1ac2da57ebd9a0",
+module.exports = {
+    APP_ID: "wxea1ac2da57ebd9a0",
 	APP_SECRET: "c669048af98c0236b5fc26364a261b15",
 	
-	ENABLE_GM: true,  // 是否开启GM命令
-};
+    ENABLE_GM: true,  // 是否开启GM命令
+    
+	// entity state
+    ENTITY_STATE_INITED: 1,
+    ENTITY_STATE_DESTROYED: 2,
+    
+    // 平台
+    Platform: {
+        WIN: "win",
+        WECHAT: "wechat",
+	},
 
-for (var f in uconsts) {
-    if(uconsts.hasOwnProperty(f)) {
-        consts[f] = uconsts[f];
-    }
-};
+	INVALID_CHAIR: 65535, 	   //无效用户
 
-module.exports = consts;
+	Code: {
+        OK: 0,
+		FAIL: 1,
+	},
+
+	Login: {
+        OK: 200,  		// 成功
+        RELAY: 201,     // 重新登入
+        MAINTAIN: 202,  // 维护
+        FAIL: 500       // 失败
+	}
+}
